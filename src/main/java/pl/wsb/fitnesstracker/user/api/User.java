417a800be9b4a6da42ca7953ed3a4a68295454ea
Filip Żmuda <filipzmuda4@gmail.com>
2025-12-20 15:33:42 +0100

    User and UserDTO documentation in JavaDoc

--- src/main/java/pl/wsb/fitnesstracker/user/api/User.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/User.java
@@ -12,24 +12,73 @@ import java.time.LocalDate;
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
+/**
+ * Represents a user of the FitnessTracker system.
+ * <p>
+ * This class is a JPA entity mapped to the {@code users} database table.
+ * It contains basic personal data required to identify and manage a user
+ * within the system.
+ * <p>
+ * Instances of this class are persisted in the database and should not be
+ * exposed directly through the API. For API communication, {@link UserDto}
+ * or other DTO classes should be used.
+ */
 public class User {
 
+    /**
+     * Unique identifier of the user.
+     * <p>
+     * This value is generated automatically by the database.
+     * It is {@code null} for new users before they are persisted.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Nullable
     private Long id;
 
+    /**
+     * of the user
+     */
     @Column
     private String firstName;
 
+    /**
+     * last name of the user
+     */
     @Column
     private String lastName;
 
+
+    /**
+     * Birth date of the user.
+     * <p>
+     * This field is mandatory and cannot be {@code null}.
+     */
     @Column(name = "birthdate", nullable = false)
     private LocalDate birthdate;
 
+    /**
+     * Email address of the user.
+     * <p>
+     * The email must be unique across all users and is required.
+     * It can be used to identify and search for a user.
+     */
     @Column(nullable = false, unique = true)
     private String email;
+
+
+    /**
+     * Creates a new {@link User} instance.
+     * <p>
+     * This constructor should be used when creating a new user
+     * before persisting it in the database.
+     * The {@code id} is not provided here, as it is generated automatically.
+     *
+     * @param firstName first name of the user
+     * @param lastName last name of the user
+     * @param birthdate birth date of the user
+     * @param email unique email address of the user
+     */
 
     public User(
             final String firstName,
--- src/main/java/pl/wsb/fitnesstracker/user/api/UserDto.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/UserDto.java
@@ -5,7 +5,46 @@ import jakarta.annotation.Nullable;
 
 import java.time.LocalDate;
 
-public record UserDto(@Nullable Long id, String firstName, String lastName,
-                      @JsonFormat(pattern = "yyyy-MM-dd") LocalDate birthdate,
-                      String email) {}
+/**
+ * Data Transfer Object representing user details exchanged through the API.
+ * <p>
+ * This DTO is used as both request and response body for user-related endpoints.
+ * It contains full user information intended for external communication and
+ * should not be confused with the {@link User} entity, which represents
+ * the persisted database state.
+ */
+public record UserDto(
+        /**
+         * Unique identifier of the user.
+         * <p>
+         * This value is {@code null} when a new user is being created and is
+         * generated automatically by the database upon persistence.
+         */
+        @Nullable Long id,
+
+        /**
+         * First name of the user.
+         */
+        String firstName,
+
+        /**
+         * Last name of the user.
+         */
+        String lastName,
+
+        /**
+         * Birth date of the user.
+         * <p>
+         * The date is serialized and deserialized using the {@code yyyy-MM-dd} format.
+         */
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate birthdate,
+
+        /**
+         * Email address of the user.
+         * <p>
+         * The email is expected to be unique and can be used to identify the user.
+         */
+        String email
+
+) {}
 
